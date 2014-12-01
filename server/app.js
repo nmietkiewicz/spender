a746e1c2a7fa1c3e9d2b4d1597044b10b1bc0ecd
nmietkiewicz <nadia.mietkiewicz@pracuj.pl>
2014-12-01 11:16:02 +0100

    allow origin na heroku

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ if(config.seedDB) { require('./config/seed'); }
 var app = express();
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
